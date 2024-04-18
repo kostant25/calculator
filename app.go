6714508f25361ea4506808191f18286c976ff76b
Konstantin <goduskospan@gmail.com>
2024-04-18 19:03:44 +0500

    Add read func

--- app.go
+++ app.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	fmt.Println("input text:")
+	str := read()
+	fmt.Println(str)
+}
+
+func read() string {
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	return line
 }
