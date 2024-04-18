f52879ce03051953c8923598c90eb8fa587d368b
Konstantin <goduskospan@gmail.com>
2024-04-18 20:02:30 +0500

    update read, add parseToInt, add operations

--- app.go
+++ app.go
@@ -5,19 +5,45 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("input text:")
-	str := read()
-	fmt.Println(str)
+	fmt.Println("Input expression:")
+	operand1, operand2, operation := read()
+
+	switch operation {
+	case "+":
+		fmt.Println(operand1 + operand2)
+	case "-":
+		fmt.Println(operand1 - operand2)
+	case "*":
+		fmt.Println(operand1 * operand2)
+	case "/":
+		fmt.Println(operand1 / operand2)
+	}
+
 }
 
-func read() string {
+func read() (o1 int, o2 int, op string) {
 	reader := bufio.NewReader(os.Stdin)
 	line, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	return line
+	expression := strings.Split(strings.Trim(line, "\n"), " ")
+	o1 = parseToInt(expression[0])
+	o2 = parseToInt(expression[2])
+	op = expression[1]
+	return
+}
+
+func parseToInt(s string) int {
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return num
 }
